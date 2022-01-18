e2da44e2bda156d6839ccb3e6c6b8386e2b62831
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2022-01-17 18:22:47 -0800

    abc

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -23,17 +23,17 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JButton[] buttons;
 	//2 create an int variable called hiddenButton
 	int hiddenButton;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		new _02_FindTheHiddenButton().start();
 	}
 	
-	public void start() {
+	public void start() throws InterruptedException {
 		window = new JFrame("Find the Button");
 		panel = new JPanel();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		String buttonString = JOptionPane.showInputDialog("Enter a positive integer. The larger the number, the harder the game will be.");
+		String buttonString = JOptionPane.showInputDialog("Enter a positive integer. The larger the number, the harder the game will be. Easier games should have the number be anywhere from 250-1000. \n Harder games should have around 1500");
 		buttonNum = Integer.parseInt(buttonString);
 		buttons = new JButton[buttonNum];
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
@@ -55,7 +55,7 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		//11. set the JFrame to visible.
 		window.setVisible(true);
 		//12. Give the user the instructions for the game.
-		JOptionPane.showMessageDialog(null, "In this game a button will briefly present itself as the hidden button by saying 'ME'. Your goal is to click the hidden button once it goes blank.\n The number of buttons is the number you entered earlier. ");
+		JOptionPane.showMessageDialog(null, "In this game a button will briefly present itself as the hidden button by saying 'ME'. Your goal is to click the hidden button. ");
 		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
 		Random ran = new Random();
 		int randomNum = ran.nextInt(buttonNum +1);
@@ -64,9 +64,9 @@ public class _02_FindTheHiddenButton implements ActionListener{
 buttons[randomNum].setText("ME");
 		//15. Use Thread.sleep(1000); to pause the program.
 		//    Surround it with a try/catch - use Eclipse helper for this
-		
+Thread.sleep(1000);
 		//16. Set the text of the JButton located at hiddenButton to be blank.
-		
+buttons[randomNum].setText(" ");
 	}
 
 	@Override
@@ -74,7 +74,12 @@ buttons[randomNum].setText("ME");
 		JButton buttonClicked = (JButton)e.getSource();
 		
 		//17. if the hiddenButton is clicked, tell the user that they win.
-		
+		if (e.getSource() == buttons[hiddenButton]) {
+			JOptionPane.showMessageDialog(null, "You Win!!! The reward is $0.00! Enjoy");	
+		}
 		//18. else tell them to try again
+		else {
+			JOptionPane.showMessageDialog(null, "You lose. Try again. ");	
+		}
 	}
 }
